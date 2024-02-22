e28cd8b3094c53d36e2757c1097f2712fb8f42ea
Estelle Poulin <dev@inspiredby.es>
2024-02-22 10:28:27 -0500

    Update default engine

--- stabilityai/constants.py
+++ stabilityai/constants.py
@@ -4,5 +4,5 @@ DEFAULT_STABILITY_API_HOST: Final[str] = "https://api.stability.ai"
 DEFAULT_STABILITY_CLIENT_ID: Final[str] = "Stability Python SDK"
 DEFAULT_STABILITY_CLIENT_VERSION: Final[str] = "1.0.0"
 
-DEFAULT_GENERATION_ENGINE: Final[str] = "stable-diffusion-xl-beta-v2-2-2"
+DEFAULT_GENERATION_ENGINE: Final[str] = "stable-diffusion-xl-1024-v1-0"
 DEFAULT_UPSCALE_ENGINE: Final[str] = "esrgan-v1-x2plus"
